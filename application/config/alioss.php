58370b7c411608cae897a7f809a98d845c85f787
sunjiaqiang <1355049422@qq.com>
2018-09-21 22:15:58 +0800

    Update alioss.php

--- application/config/alioss.php
+++ application/config/alioss.php
@@ -7,8 +7,8 @@
  * @description 阿里oss配置文件
  */
 $config['oss'] = [
-    'accessKeyId'=>'LTAIOJA6Xgm7yCFg',
-    'accessKeySecret'=>'D0TVCoohM21Z74nJJhoom3eo8bxJbz',
-    'endpoint'=>'oss-cn-beijing.aliyuncs.com',
-    'bucket'=>'lb-images'
+    'accessKeyId'=>'<您从OSS获得的AccessKeyId>',
+    'accessKeySecret'=>'<您从OSS获得的AccessKeySecret>',
+    'endpoint'=>'<您选定的OSS数据中心访问域名，例如oss-cn-hangzhou.aliyuncs.com>',
+    'bucket'=>'您创建的bucket名称'
 ];
